d8ff083cd0a99b3ac5f32bcb635cf5853d2336eb
mattalhamilton <79939136+mattalhamilton@users.noreply.github.com>
2021-04-28 18:22:17 -0500

    Add files via upload

--- Mod12H.ps1
+++ Mod12H.ps1
@@ -26,30 +26,38 @@ foreach ($line in Get-Content $file)
     {
         continue
     }
-    $split1 = $line.split("[**]")
-    $date_time = $split1[0]
-    $date = $date_time[0..5]
-    $time = $date_time[6..11]
-    $split2 = $split1[1]
-    $split2 = $split2.split("]")
-    $description = $split2[1]
-    $split3 = $split2[2]
-    $split3 = $split3.split("] [")
-    $classification = $split3[0]
-    $classification = $classification
-    $classification = $classification[16..400]
-    $split4 = $split3[1]
-    $split4 = $split4.split("] ")
-    $priority = $split4[0]
-    $priority = $priority[-1]
-    $split5 = $split4[1].split("} ")
-    $ip_addresses = $split5[1]
-    $ip_addresses = $ip_addresses.split[" -> "]
-    $source_ip = $ip_addresses[0]
-    $destination_ip = $ip_addresses[1]
+    $line = $line.Substring(0)
+    $split1 = $line.Substring(0)
+    $split1 = $split1.Substring(0)
+    $split1 = $split1.Split("[**]")
+    $date_time = $split1.Substring(0)
+    $date = $date_time.Substring(0)
+    $date = $date[0].Substring(0,5)
+    $time = $date_time[0].Substring(6,11)
+    $split2 = $line.Substring(0)
+    $split2 = $split2.Split("]")
+    $split2 = $split2[2]
+    $description = $split2.Trim(" [**")
+    $split3 = $line.Substring(0)
+    $split3 = $split3.Split("]")
+    $classification = $split3[3]
+    $classification = $classification.Trim("[Classification: ")
+    $split4 = $line.Substring(0)
+    $split4 = $split4.Split("[Priority: ")
+    $Priority
+
+    #$split4 = $split3.substring[1]
+    #$split4 = $split4.split("] ")
+    #$priority = $split4.substring[0]
+    #$priority = $priority.substring[-1]
+    #$split5 = $split4.substring[1].split("} ")
+    #$ip_addresses = $split5.substring[1]
+    #$ip_addresses = $ip_addresses.split[" -> "]
+    #$source_ip = $ip_addresses.substring[0]
+    #$destination_ip = $ip_addresses.substring[1]
     
-    write-Host "$split2"
-    write-Host "$date + "," + $time + "," + $priority + "," + $classification + "," + $description + "," + $source_ip + "," + $destination_ip"
+    
+    write-Host "$split4"
     #Write-Output ($date + "," + $time + "," + $priority + "," + $classification + "," + $description + "," + $source_ip + "," + $destination_ip) | Out-File alert_data.csv -Encoding ascii -Append
     #Write-Output $line_out | Out-File alert_data.csv -Encoding ascii -Append
 }
